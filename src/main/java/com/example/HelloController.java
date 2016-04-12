084555fc79000d9496a780d75b656bfd431c98dd
Tanya Powell <hello@tanyapowell.co.uk>
2016-04-12 22:55:47 +0100

    initial hello twitter world commit

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.twitter.api.CursoredList;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.TwitterProfile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    private Twitter twitter;
+    private ConnectionRepository connectionRepository;
+
+    @Inject
+    public HelloController (Twitter twitter, ConnectionRepository connectionRepository) {
+        this.twitter = twitter;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String helloTwitter (Model model) {
+        if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
+            return "redirect:/connect/twitter";
+        }
+
+        model.addAllAttributes((Map<String, ?>) twitter.userOperations().getUserProfile());
+        CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
+        model.addAttribute("friends", friends);
+
+        return "hello";
+    }
+}
